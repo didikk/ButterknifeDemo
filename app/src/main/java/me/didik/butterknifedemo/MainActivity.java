0ad657c09743570a5bf1185d2ad1e4e68e1e812e
Didik Ismawanto <ismawantodidik@gmail.com>
2016-12-11 18:40:29 +0700

    Change to latest version

--- app/src/main/java/me/didik/butterknifedemo/MainActivity.java
+++ app/src/main/java/me/didik/butterknifedemo/MainActivity.java
@@ -2,35 +2,53 @@ package me.didik.butterknifedemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import butterknife.Bind;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.Unbinder;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getName();
 
-    // Contoh deklarasi edit text. Butterknife otomatis mengenali tipe komponen yang kita buat.
-    @Bind(R.id.my_edit_text)
-    EditText myEditText;
+    @BindView(R.id.et_name) EditText etName;
+    @BindView(R.id.et_email) EditText etEmail;
+    @BindView(R.id.et_password) EditText etPassword;
+    @BindView(R.id.et_confirm_password) EditText etConfirmPassword;
+    @BindView(R.id.et_address) EditText etAddress;
+    @BindView(R.id.et_phone) EditText etPhone;
+
+    private Unbinder unbinder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Jangan lupa memanggil method ini agar komponen yang kita buat bisa dikenali
-        ButterKnife.bind(this);
+        unbinder = ButterKnife.bind(this);
     }
 
-    /**
-     * Untuk menghandel onClick sebuah button tinggal gunakan anotasi @OnClick(id button) dan diikuti sebuah fungsi
-     * Button tidak perlu dideklarasikan terlebih dahulu seperti edit text di atas.
-     */
-    @OnClick(R.id.my_button)
-    void buttonClicked() {
-        String name = myEditText.getText().toString();
-        Toast.makeText(MainActivity.this, "Hello " + name, Toast.LENGTH_SHORT).show();
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+    }
+
+    @OnClick(R.id.btn_submit)
+    public void onClick() {
+        String name = etName.getText().toString();
+        String mail = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        String confirmPassword = etConfirmPassword.getText().toString();
+        String address = etAddress.getText().toString();
+        String phone = etPhone.getText().toString();
+
+        String form = name + "\n" + mail + "\n" + password + "\n" + confirmPassword + "\n" +
+                address + "\n" + phone;
+        Log.d(TAG, form);
+
+        Toast.makeText(this, "Registration success.", Toast.LENGTH_SHORT).show();
     }
 }
